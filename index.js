e45d5f4fda27bd9515a96f541299d8a220d8c25f
Isaac Avilez <isaac1180@aol.com>
2020-02-18 16:59:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
   drivers.push(name);
-  drivers.unshift(name);
-  drivers.pop(name);
-  drivers.shift(name); 
+  
   
 }
